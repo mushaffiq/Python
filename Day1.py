db019c157cae2e30e1c650cdcf4cf453d0ffce36
mushaffiq <40059574+mushaffiq@users.noreply.github.com>
2024-08-25 16:47:40 +0530

    Add files via upload

---
+++ Day1.py
@@ -0,0 +1,22 @@
+print("Hello World !")
+print("Hello\nworld! ")
+print("Hello " + input("What's Your Name ?")+"!")
+# name = input("What is your name?")
+# print(name)
+#
+# name = "James"
+# print(name)
+
+# name = "Angela"
+# print(len(name))
+
+
+# Mam  Method
+Count = input("What is Your Name ")
+length = len(Count)
+print(length)
+
+username_of_the_length = "Angela"
+length = len(username_of_the_length)
+print(length)
+
